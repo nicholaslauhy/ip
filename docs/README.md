81fc479d1c616b77782e3742cc9abd97e513e122
Nicholas Lau <nicholaslauhongyi@gmail.com>
2026-01-19 00:04:51 +0800

    Remove Duke, Add Lumi
    Current default setup made
    
    Want to create a new personality and new name
    
    Remove Duke from Duke.java and create Lumi,
    refactoring all Duke elements to Lumi
    
    Want to make sure that there is no trace
    of Duke

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Lumi User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Lumi.java
@@ -0,0 +1,14 @@
+public class Lumi {
+    public static void main(String[] args) {
+        String logo =
+                " _      _   _   __  __ ___ \n"
+                        + "| |    | | | | |  \\/  |_ _|\n"
+                        + "| |    | | | | | |\\/| || | \n"
+                        + "| |___ | |_| | | |  | || | \n"
+                        + "|_____| \\___/  |_|  |_|___|\n";
+        System.out.println("Hello I am\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println();
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
