d4bab43bd20191b90c43ef8829f13888930caaba
Nicholas Lau <nicholaslauhongyi@gmail.com>
2026-01-28 18:37:57 +0800

    Echo user, exit on Bye
    Currently, Lumi is only able to show its
    character.
    
    However, it is not able to exchange words with
    the user.
    
    Lumi echo user, and exits on bye
    
    This is to create a simple interface for interaction.
    To make it more personalised, we asked for user's name.
    More complicated features will be created in later commits.

--- src/main/java/Lumi.java
+++ src/main/java/Lumi.java
@@ -1,4 +1,10 @@
+import java.util.Scanner;
 public class Lumi {
+
+    // create divider
+    private static final String DIVIDER =
+            "____________________________________________________________";
+
     // create a list of introductions that Lumi can use instead
     private static final String[] INTROS = {
         "Hey! Lumi here, What can I help you with?",
@@ -28,16 +34,46 @@ public class Lumi {
         int idx = (int)(Math.random() * GOODBYE.length);
         return GOODBYE[idx];
     }
+
     public static void main(String[] args) {
+        // For user input
+        Scanner in = new Scanner(System.in);
+
+        // Ask for user's name
+        System.out.print("What's your name? ");
+        String name = in.nextLine().trim();
+
+        // If user's name is empty, fallback name
+        if (name.isEmpty()) {
+            name = "You";
+        }
+
         String logo =
                   " _      _   _   __  __ ___ \n"
                 + "| |    | | | | |  \\/  |_ _|\n"
                 + "| |    | | | | | |\\/| || | \n"
                 + "| |___ | |_| | | |  | || | \n"
                 + "|_____| \\___/  |_|  |_|___|\n";
-        System.out.println("Hello I am\n" + logo);
+        System.out.println("Hello " + name.toUpperCase() + ", I am\n" + logo);
         System.out.println(getIntro());
-        System.out.println();
-        System.out.println(getGoodBye());
+        System.out.println(DIVIDER);
+
+        while (true){
+            System.out.print(name + ": ");
+            String user_input = in.nextLine();
+
+            // Exit condition when user says bye
+            if (user_input.equalsIgnoreCase("bye")){
+                System.out.println(DIVIDER);
+                System.out.println("Lumi: " + getGoodBye());
+                System.out.println(DIVIDER);
+                break;
+            }
+
+            // Echo input
+            System.out.println(DIVIDER);
+            System.out.println("Lumi: " + user_input);
+            System.out.println(DIVIDER);
+        }
     }
 }
